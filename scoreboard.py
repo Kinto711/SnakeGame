0ae85003d8ccb92c363c35a9ed76767b5b1bd966
Alexander Pashkou <64390864+Kinto711@users.noreply.github.com>
2022-10-22 12:14:22 -0400

    Snake Game + Score tracking from local file

--- scoreboard.py
+++ scoreboard.py
@@ -5,23 +5,38 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("score.txt") as data:
+            self.record = int(data.read())
         self.create_banner()
 
     def create_banner(self):
+
         self.hideturtle()
         self.penup()
         self.color("white")
-        self.goto(0, 270)
+        self.goto(-80, 270)
         self.write(f"Score: {self.score}", False, align="center", font=('Arial', 25, 'normal'))
+        self.goto(80, 270)
+        self.write(f"score: {self.record}", False, align="center", font=('Arial', 25, 'normal'))
 
     def update(self):
         self.clear()
         self.score += 1
+        self.goto(-80, 270)
         self.write(f"Score: {self.score}", False, align="center", font=('Arial', 25, 'normal'))
-        print("+1 to score")
-        print(self.score)
+
+        if self.score > self.record:
+            self.record = self.score
+            with open("score.txt", 'w') as f:
+                f.write(str(self.record))
+        self.goto(80, 270)
+        self.write(f"score: {self.record}", False, align="center", font=('Arial', 25, 'normal'))
+        # print(f"{self.score} + score")
+        # print(f"{self.record.read()} Record")
+
 
 
     def game_over(self):
         self.goto(0, 0)
         self.write(f"GAME OVER", False, align="center", font=('Arial', 60, 'normal'))
+
